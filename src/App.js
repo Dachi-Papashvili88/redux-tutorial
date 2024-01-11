5cfb20e026df6c75f1a01f3d7855197473aeea0a
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2024-01-11 10:25:07 +0000

    create Modal component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useEffect } from 'react';
-import Navbar from './components/Navbar';
-import CartContainer from './components/CartContainer';
-import { useSelector, useDispatch } from 'react-redux';
-import { calculateTotals } from './features/cart/cartSlice';
+import { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 function App() {
   const { cartItems } = useSelector((state) => state.cart);
@@ -12,6 +13,7 @@ function App() {
   }, [cartItems]);
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
